496ff7be518d88f3eaba212a9c58cae2d8a51e6f
王鹏 <peng.wang09@hand-china.com>
2019-08-12 11:32:49 +0800

    [feat]修改Readme

--- LeetCode/ByteDance/String/1.js
+++ LeetCode/ByteDance/String/1.js
@@ -4,24 +4,59 @@
 
 // 算法思想：遍历循环字符串，若存在俩个相同字母，则continue（跳过本次迭代）,记录len值，继续循环，最后比对len
 
-// 比对len发现，无法保存多个间隔的len值，比如‘abcccdeffghjkkkkiou’,即需比对abc,cdef,fghjk,kiou
-var lengthOfLongestSubstring = function(s) {
-    if(s == null){
-        return 0
-    }
-    if (s.length === 1) {
-        return 1   
-    }
-    let len = 0;
-    for (let i =0; i<s.length; i++){
+// 用双层循环
 
-        for (let j = i; j<s.length; j++){
-            if(s[j+1] === s[j]){
-                i = j;
-                continue;
-            }
-            len = s.length > len ? s.length : len;
+var lengthOfLongestSubstring = function(s) {
+    const arr = [];
+    for (var i = 0; i < s.length; i++) {
+      arr[i] = [];
+      for (var j = i; j < s.length; j++) {
+        if (arr[i].indexOf(s[j]) > -1) {
+          break;
+        } else {
+          arr[i].push(s[j]);
         }
+      }
     }
-    return len
- };
+    console.log('arr', arr)
+    var max = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].length > max) {
+        max = arr[i].length;
+      }
+    }
+    return max;
+  };
+
+  lengthOfLongestSubstring('abcabcbb')
+//  // 单次循环，利用ES6的 Set函数
+//  var lengthOfLongestSubstring = function(s) {
+//     let start = 0
+//     let end = 1
+//     for (let i=0; i<s.length; i++) {
+//       let temp = new Set(s.slice(start, end).split(''))
+//       if (temp.size !== end - start) {
+//         start += 1
+//       }
+//       end++
+//     }
+//     return end - start - 1
+// };
+
+// // 单词循环 利用js内置函数处理
+// var lengthOfLongestSubstring = function(s) {
+//     var str = '';
+//              var len = 0;
+//              for(var i=0;i<s.length;i++){
+//                  var char = s.charAt(i);
+//                  var index = str.indexOf(char);
+//                  if(index === -1){ 
+//                      str += char;
+//                      len = len<str.length?str.length:len;
+//                  }
+//                  else{
+//                      str = str.substr(index + 1) + char;
+//                  }
+//              }
+//              return len;
+//  };
