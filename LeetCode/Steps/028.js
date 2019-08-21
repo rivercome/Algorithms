4ed38ff57067a827cb07b48d275d1d69aa05313d
王鹏 <peng.wang09@hand-china.com>
2019-08-21 17:06:31 +0800

    [0821]

---
+++ LeetCode/Steps/028.js
@@ -0,0 +1,10 @@
+/* 十分讨巧，算了算了 */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
