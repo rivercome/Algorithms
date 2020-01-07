77f037066a502dcf8a88299c0faa3a6f78cffa69
wangpeng <897971480@qq.com>
2020-01-07 16:47:46 +0800

    [feat]14，16

---
+++ LeetCode/剑指offer/14链表中倒数第K个结点.js
@@ -0,0 +1,23 @@
+    /* function ListNode(x){
+    this.val = x;
+    this.next = null;
+    }*/
+    // 尺子，双指针构建一个尺子去移动
+    function FindKthToTail(head, k) {
+        if (head === null || k <= 0) return null;
+        let pNode1 = head,
+          pNode2 = head;
+        while (--k) {
+          if (pNode2.next !== null) {
+            pNode2 = pNode2.next;
+          } else {
+            return null;
+          }
+        }
+        while (pNode2.next !== null) {
+          pNode1 = pNode1.next;
+          pNode2 = pNode2.next;
+        }
+        return pNode1;
+      }
+  
